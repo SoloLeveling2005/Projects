373ed7b85438fc9964c54da5f753f48e803ccea5
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2022-11-17 11:06:23 +0600

    17.11.2022

--- Messenger/twitter_app/frontend/src/assets/js/home/main.js
+++ Messenger/twitter_app/frontend/src/assets/js/home/main.js
@@ -1,6 +1,6 @@
 
 
-
+// Контроль размер меню и main блока
 var element = document.querySelector(".left-menu");
 var marginLeft = parseInt(getComputedStyle(element, true).marginLeft);
 var marginRight = parseInt(getComputedStyle(element, true).marginRight);
@@ -17,33 +17,33 @@ window.addEventListener('resize', function(event) {
 }, true);
 
 
-function randomIntFromInterval(min, max) { 
-// min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
+// function randomIntFromInterval(min, max) {
+// // min and max included
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+// }
 
 
-function new_tweet() {
-    let tweet_text = document.querySelector(".tweet_text").value
-    let tweet_id = String(+(new Date())).substr(6, 200) + randomIntFromInterval(100, 999)
-    let author_id = getCookie('user_id')
-    console.log(tweet_text)
-    console.log(tweet_id)
-    console.log(author_id)
-    fetch("new_tweet/", {
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
-          "X-CSRFToken": getCookie("csrftoken"),
-        },
-        body: JSON.stringify({id_tweet: tweet_id,author_id:author_id,tweet_text:tweet_text})
-      })
-      // .then(response => response.json())
-      // .then(data => {
-      //
-    // });
-    console.log("Запрос прошел успешно");
-    document.querySelector(".tweet_text").value = ""
-}
-
+// function new_tweet() {
+//     let tweet_text = document.querySelector(".tweet_text").value
+//     let tweet_id = String(+(new Date())).substr(6, 200) + randomIntFromInterval(100, 999)
+//     let author_id = getCookie('user_id')
+//     console.log(tweet_text)
+//     console.log(tweet_id)
+//     console.log(author_id)
+//     fetch("new_tweet/", {
+//         method: "POST",
+//         credentials: "same-origin",
+//         headers: {
+//           "X-Requested-With": "XMLHttpRequest",
+//           "X-CSRFToken": getCookie("csrftoken"),
+//         },
+//         body: JSON.stringify({id_tweet: tweet_id,author_id:author_id,tweet_text:tweet_text})
+//       })
+//       // .then(response => response.json())
+//       // .then(data => {
+//       //
+//     // });
+//     console.log("Запрос прошел успешно");
+//     document.querySelector(".tweet_text").value = ""
+// }
+//
