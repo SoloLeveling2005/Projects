670e1164230fde80d684dcbfd5c5e35c1ff4d583
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2022-11-19 18:24:49 +0600

    19.11.2022

--- Messenger/todo_app/urls.py
+++ Messenger/todo_app/urls.py
@@ -12,8 +12,11 @@ urlpatterns = [
     path('home/<int:user_id>/new_task', views.new_task, name='new_task'),
     path('home/<int:task_id>/complete_task', views.complete_task, name='complete_task'),
     path('home/<int:task_id>/delete_task', views.delete_task, name='delete_task'),
+    path('home/<int:task_id>/task_detail', views.task_detail, name='task_detail'),
     # path('my_api_one/<int:post_id>/', views.my_api_one),
     # path('my_api/', views.my_api),
+    path('get_example_tasks/', views.get_example_tasks),
+
 ]
 
 
--- Messenger/todo_app/views.py
+++ Messenger/todo_app/views.py
@@ -15,19 +15,50 @@ from todo_app.models import Tasks
 from log_auth.models import Users
 
 
+def get_example_tasks(request: HttpRequest):
+    data = [[x * y * z for x in range(1, 6)] for y in range(1, 6) for z in range(1, 6)]
+    return JsonResponse({'data': data})
 
-def complete_task(request: HttpRequest, task_id:int):
-    if request.method == "GET":
-        author_id = Tasks.objects.get(id=task_id).author_id
-        task = Tasks.objects.get(id=task_id)
-        task.done = True
-        task.save()
-        return redirect(reverse('todo_app:home', args=(author_id,)))
-    
-def delete_task(request: HttpRequest, task_id: int):
+
+def get_one_task(task_id):
+    return Tasks.objects.get(id=task_id)
+
+
+def get_all_tasks():
+    return Tasks.objects.all()
+
+
+def task_list(request: HttpRequest, task_id: int):
     pass
 
-def new_task(request: HttpRequest, user_id:int):
+
+def task_detail(request: HttpRequest, task_id: int):
+    task = get_one_task(task_id)
+    if request.method == "GET":
+        return render(request, 'public/todo_app/one.html', context={'content': task})
+
+
+def complete_task(request: HttpRequest, task_id: int):
+    if request.method == "GET":
+        author_id = get_one_task(task_id).author_id
+        task = get_one_task(task_id)
+        task.done = True
+        task.save()
+        with open("logs.txt", "a") as myfile:
+            myfile.write(f"- Пользователь {author_id} выполнил задачу {task_id}\n")
+        return redirect(reverse('todo_app:home', args=(author_id,)))
+
+
+def delete_task(request: HttpRequest, task_id: int):
+    if request.method == "GET":
+        author_id = Tasks.objects.get(id=task_id).author_id
+        Tasks.objects.get(id=task_id).delete()
+        with open("logs.txt", "a") as myfile:
+            myfile.write(f"- Пользователь {author_id} удали задачу {task_id}\n")
+        return redirect(reverse('todo_app:home', args=(author_id,)))
+
+
+def new_task(request: HttpRequest, user_id: int):
     if request.method == "POST":
         title = request.POST.get('title', "")
         description = request.POST.get('description', "")
@@ -43,7 +74,10 @@ def new_task(request: HttpRequest, user_id:int):
             task_description=description,
             done=False
         )
+        with open("logs.txt", "a") as myfile:
+            myfile.write(f"- Пользователь {user_id} создал задачу\n")
         return redirect(reverse('todo_app:home', args=(user_id,)))
+
 
 def home(request: HttpRequest, user_id: int = 0) -> HttpResponse:
     if request.method == "GET":
@@ -53,8 +87,12 @@ def home(request: HttpRequest, user_id: int = 0) -> HttpResponse:
             return redirect(reverse('log_auth:log_auth', args=()))
         else:
             if int(cookies_user_id) == int(user_id):
-                todos = list(Tasks.objects.all().values())
+                todos = list(get_all_tasks().values())
                 print(todos)
-                return render(request, 'public/todo_app/home.html', context={'user_id': cookies_user_id, 'content': todos})
+
+                return render(request, 'public/todo_app/home.html',
+                              context={'user_id': cookies_user_id, 'content': todos})
             else:
+                with open("logs.txt", "a") as myfile:
+                    myfile.write("Начало работы\n")
                 return redirect(reverse('todo_app:home', args=(cookies_user_id,)))
