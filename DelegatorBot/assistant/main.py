e83817427dbff85d20bf1ec4135994b831d77ea5
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2023-02-17 15:52:39 +0600

    17.02.2023

--- DelegatorBot/assistant/main.py
+++ DelegatorBot/assistant/main.py
@@ -8,8 +8,7 @@ import pyaudio
 from vosk import Model, KaldiRecognizer
 from text_voiceover import Voice
 from core import Core
-
-
+import time
 
 
 class Assistant(Core):
@@ -18,6 +17,7 @@ class Assistant(Core):
         model = Model(f'vosk-model-small-ru-0.4')
         self.data_listen = ""
         self.voice = Voice()
+        self.end_time = time.perf_counter()
 
         self.rec = KaldiRecognizer(model, 16000)
         self.p = pyaudio.PyAudio()
@@ -25,36 +25,47 @@ class Assistant(Core):
         self.stream.start_stream()
         self.go_listen()
 
-
-
     def listen(self):
         while True:
-            data = self.stream.read(9000, exception_on_overflow=False)
-            if (self.rec.AcceptWaveform(data)) and (len(data) > 0):
-                answer = json.loads(self.rec.Result())
-                if answer['text']:
-                    yield answer['text']
+            time_now = time.perf_counter()
+
+            if time_now - self.end_time > 8:
+                self.end_time = time_now
+                self.data_listen = ""
+                print("end")
+            else:
+                print(time_now - self.end_time)
+                data = self.stream.read(10000, exception_on_overflow=False)
+                if (self.rec.AcceptWaveform(data)) and (len(data) > 0):
+                    answer = json.loads(self.rec.Result())
+                    if answer['text']:
+                        yield answer['text']
 
     def go_listen(self):
         for text in self.listen():
-            text = (text.strip()) \
-                .replace("плюс", "+") \
-                .replace("минус", "-") \
-                .replace("один", "1") \
-                .replace("два", "2") \
-                .replace("три", "3") \
-                .replace("четыре", "4") \
-                .replace("пять", "5") \
-                .replace("шесть", "6") \
-                .replace("семь", "7") \
-                .replace("восемь", "8") \
-                .replace("девять", "9")
+            # text = (text.strip()) \
+            #     .replace("плюс", "+") \
+            #     .replace("минус", "-") \
+            #     .replace("один", "1") \
+            #     .replace("два", "2") \
+            #     .replace("три", "3") \
+            #     .replace("четыре", "4") \
+            #     .replace("пять", "5") \
+            #     .replace("шесть", "6") \
+            #     .replace("семь", "7") \
+            #     .replace("восемь", "8") \
+            #     .replace("девять", "9")
             self.data_listen += text.strip() + " "
             # print(text)
-            # print(self.data_listen)
-            if self.coincidence(self.data_listen.lower()):
-                self.data_listen = ""
+            print(self.data_listen)
+            # if self.coincidence(self.data_listen.lower()):
+            #     self.data_listen = ""
 
+            # Здесь можно выполнить ваш код
+
+            self.end_time = time.perf_counter()
+
+            # execution_time = end_time - start_time
 
             # str1 = 'Python'
             # str2 = 'Pthon'
---
+++ Hello/core/admin.py
@@ -0,0 +1,70 @@
+from django.contrib import admin
+from core import models
+
+
+
+
+class Users(admin.ModelAdmin):
+    """
+    Настройки отображения, фильтрации и поиска модели:'Todo' на панели администратора
+    """
+
+    list_display = (
+        'username',
+        'gmail',
+        'password',
+        'created_at',
+        'ico',
+        'phone',
+        'gender',
+        'birthday'
+
+    )
+    list_display_links = (
+        'username',
+        'gmail',
+        'password',
+        'created_at',
+        'ico',
+        'phone',
+        'gender',
+        'birthday'
+    )
+    list_editable = (
+
+    )
+    list_filter = (
+        'username',
+        'gmail',
+        'password',
+        'created_at',
+        'ico',
+        'phone',
+        'gender',
+        'birthday'
+    )
+    fieldsets = (
+        ('Основное', {'fields': (
+            'username',
+            'gmail',
+            'password',
+            'created_at',
+            'ico',
+            'phone',
+            'gender',
+            'birthday'
+        )}),
+    )
+    search_fields = [
+        'username',
+        'gmail',
+        'password',
+        'created_at',
+        'ico',
+        'phone',
+        'gender',
+        'birthday'
+    ]
+
+
+admin.site.register(models.Users, Users)
