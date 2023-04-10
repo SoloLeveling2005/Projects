443d52e25522e4d8e39e80a97334fedb4b68da93
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2023-04-10 06:36:41 +0600

    AI

--- AI/Сore.py
+++ AI/Сore.py
@@ -1,10 +1,20 @@
-mass_
-
-
+import sqlite3
 
 
 class Core:
-    def __init__(self):
-        self.name = ""
-        self.ages = 0
+    def __init__(self, name: str = "ChatBot", path: str = "./"):
+        self.name = name
+        self.bot = sqlite3.connect(f"{path}{name}.db")
+        self.token = sqlite3.connect(f"{path}token.db")
+
+    def train(self, text_data: str = ""):
+        pass
+
+    def speak(self, question: str = ""):
+        pass
+
+
+
+
+
 
